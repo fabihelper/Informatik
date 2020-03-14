bb77e7afb502f804dc87cfc01a5be8118524596e
fabihelper <fabian.scheuch@live.de>
2020-03-14 19:14:54 +0100

    Stern-Dreieck mit User-Eingabe added

--- Aufgabe01/src/Aufgabe1.java
+++ Aufgabe01/src/Aufgabe1.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
+
 
 public class Aufgabe1 
 {
 	public static void main(String[] args)
 	{
-		System.out.print("Hello World");
+		
+		System.out.print("Anzahl der Reihe bitte: ");
+		Scanner reihe = new Scanner(System.in);
+		int reihen = reihe.nextInt();
+		//System.out.print("User input " + reihe.nextLine());
+		
+		for(int i = 1; i <= reihen; ++ i)
+		{
+			for(int j = 1; j <= reihen ; ++j)
+			{
+				if(j <= reihen - i)
+				{
+					System.out.printf(" ");
+				}
+				else
+				{
+					System.out.printf("*");
+				}
+			}
+			System.out.printf("\n");
+		}
+		
+		
+		
 	}
 
 }
