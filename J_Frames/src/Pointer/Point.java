1588051e0f5dd3d2cdf44357f5e6e2ddb4c032d6
fabihelper <fabian.scheuch@live.de>
2020-03-17 20:26:37 +0100

    Test

---
+++ J_Frames/src/Pointer/Point.java
@@ -0,0 +1,48 @@
+package Pointer;
+import java.awt.*;
+
+public class Point {
+		Point(int x, int y)
+	{
+		
+	}
+
+	double distance(Point punkt)
+	{
+		return 8;
+		
+	}
+	
+	
+	
+	void setLocation(int x, int y)
+	{
+		
+	}	
+	
+	
+	void setLocation(double x, double y)
+	{
+		
+	}
+	
+	void translate(int x, int y)
+	{
+		
+	}
+	
+	public static void main(String[] args)
+	{
+		int x; 
+		int y;
+		
+		Point punkt1 = new Point(5, 4);
+		Point punkt2 = new Point(8, 7);
+		
+	}
+	
+
+	
+	
+
+}
